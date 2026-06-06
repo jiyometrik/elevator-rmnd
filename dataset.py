b444e672af46007a040ca810f1a65ae4520b2b64
jiyometrik <darrenyap08@gmail.com>
2026-06-06 09:19:54 +0800

    more bogus data created

--- dataset.py
+++ dataset.py
@@ -3,92 +3,35 @@ dataset.py
 Generate a fake lift dataset for testing future lift models
 """
 
+import datetime
 import os
-import random
+from typing import Literal
 
 import numpy as np
 import pandas as pd
-from netron import start
 
 OUTPUT_DIR = os.path.dirname(os.path.abspath(__file__))
-N_SAMPLES = 2 * 100
-N_STORIES = 4
+START_DATE = datetime.datetime(2026, month=6, day=1)  # can be changed
+END_DATE = datetime.datetime(2026, month=7, day=1)
 
-np.random.seed(67)
+np.random.seed(43)
 
-
-def generate_dataset(
-    n_samples: int = N_SAMPLES, n_stories: int = N_STORIES
-) -> pd.DataFrame:
-    """
-    Generates the fake lift dataset and returns a pandas.DataFrame, with the following columns:
-    - `start_storey`: an integer representing the storey at which the lift starts its ascent/descent
-    - `end_storey`: an integer representing the storey at which the lift starts its ascent/descent
-    - `microswitch_state`: a Boolean representing the state of brake armature microswitches
-    - `armature_dist`: a float representing the distance between the armature and brake pad
-    - `brakecoil_current`: a float representing the current in the brake coil
-    - `door_dist`: a float representing the distance between the cab doors
-    - `door_dist_dot`: a float representing the rate of change of the distance between the cab doors
-    - `floor_dist`: a float representing the distance between the cab floor and the landing floor
-    - `rope_tension`: a float representing the tension in the steel rope
-    - `rope_flux`: a float representing the magnetic flux leak from the steel rope
-    - `bearing_temp`: a float representing the ambient temperature surrounding the ball bearings
-    """
-    # Generate `start_storey` and `end_storey`, but make sure `start_storey[i]` != `end_storey[i]` ∀i
-    start_stories = np.ones(n_samples) + np.random.randint(n_stories, size=n_samples)
-    end_stories = np.mod(
-        np.ones(n_samples) + np.abs(start_stories + np.random.choice([-2, -1, 1, 2])),
-        n_stories,
-    )
-
-    assert all(i != 0 for i in start_stories - end_stories)
-
-    # Generate `microswitch_state`: 1 = "open", 0 = "closed"
-    microswitch_states = np.random.randint(2, size=n_samples)
-    # Generate `armature_dist`, in mm, scaled to [0, 10]
-    armature_dists = 10 * np.random.chisquare(1, size=n_samples)
-    armature_dists /= armature_dists.max()
-    # Generate `brakecoil_current`, in A, scaled to [0.5, 5]
-    brakecoil_currents = np.random.rayleigh(2, size=n_samples)
-    # Generate `door_dist` in mm, scaled to [0, 10]
-    door_dists = np.random.chisquare(3, size=n_samples) ** 1.1
-    door_dists = np.sort(door_dists)
-    door_dist_dots = np.gradient(door_dists)
-
-    # Generate `floor_dist` in mm, scaled to [0, 10]
-    floor_dist = np.random.chisquare(3, size=n_samples) ** 1.03
-
-    # Generate `rope_tension` in kN, scaled to [5, 21]
-    rope_tensions = 5 * np.random.wald(3, 2.5, size=n_samples)
-
-    # Generate `rope_flux` in mT, scaled to [0, 20]
-    rope_fluxes = np.random.pareto(3, size=n_samples)
-
-    # Generate `bearing_temp` in deg C, scaled to [30, 70]
-    bearing_temps = (
-        10 * np.random.noncentral_chisquare(1, 2.5, size=n_samples) ** 1 + 25
-    )
-
-    # Generate empty list for urgency levels
-    urgency_levels = np.zeros_like(n_samples)
-
-    df = pd.DataFrame(
-        {
-            "start_storey": start_stories.astype(int),
-            "end_storey": end_stories.astype(int),
-            "microswitch_state": microswitch_states.astype(bool),
-            "armature_dist": armature_dists,
-            "brakecoil_current": brakecoil_currents,
-            "door_dist": door_dists,
-            "door_dist_dot": door_dist_dots,
-            "floor_dist": floor_dist,
-            "rope_tension": rope_tensions,
-            "rope_flux": rope_fluxes,
-            "bearing_temp": bearing_temps,
-            "urgency": urgency_levels,
-        }
-    )
-    return df
+WEIGHTS = {
+    "arm_dist": 0.3,
+    "door_dist": 0.1,
+    "floor_dist": 0.2,
+    "rope_mfl": 0.3,
+    "bearing_temp": 0.1,
+}
+AMBIENT_TEMP = 28
+THRESHOLDS = {
+    "arm_dist": 0.65,
+    "door_dist": 6,
+    "floor_dist": 8,
+    "rope_mfl": 3,
+    "bearing_temp": 30 + AMBIENT_TEMP,
+    "dsi": 1,
+}
 
 
 def export_dataset(df: pd.DataFrame, fname: str = "lift_data.csv") -> None:
@@ -98,6 +41,98 @@ def export_dataset(df: pd.DataFrame, fname: str = "lift_data.csv") -> None:
     df.to_csv(os.path.join(os.path.dirname(__file__), fname))
 
 
+def get_dsi(
+    entry: dict, weights: dict = WEIGHTS, thresholds: dict = THRESHOLDS
+) -> float:
+    """
+    Aggregates the DSI (Degradation Severity Index) for a given log entry,
+    using the specified weights and thresholds for each component.
+    """
+    return sum(
+        entry[component] / thresholds[component] * weight
+        for component, weight in weights.items()
+    )
+
+
+def generate_entry(
+    timestamp: datetime.datetime,
+    start_date: datetime.datetime = START_DATE,
+    end_date: datetime.datetime = END_DATE,
+) -> dict:
+    """
+    Generate a single log entry for the lift dataset, given a timestamp.
+    The log entry contains the following components:
+    - `timestamp': the date and time of the log entry
+    - `arm_dist': the distance between the arm and the shaft in mm
+    - `door_dist': the door gap in mm
+    - `floor_dist': the distance between the cab floor and landing in mm
+    - `rope_mfl': the magnetic flux leakage of the rope in mT
+    - `bearing_temp': the temperature of the bearing in deg C
+    - `dsi`: the degradation severity index, given by the `get_dsi` function
+    """
+    time_elapsed = (timestamp - start_date).total_seconds() / (
+        end_date - start_date
+    ).total_seconds()
+    arm_dist = min(max(0, np.random.normal(loc=0.5 * time_elapsed, scale=0.1)), 1.0)
+    door_dist = min(max(0, np.random.normal(loc=3.9 * time_elapsed, scale=0.5)), 7.8)
+    floor_dist = min(max(0, np.random.normal(loc=4.45 * time_elapsed, scale=0.5)), 8.9)
+    rope_mfl = min(max(0, np.random.normal(loc=25 * time_elapsed, scale=5)), 50)
+    bearing_temp = min(
+        max(32, np.random.normal(loc=62 * time_elapsed + 32, scale=5)), 75
+    )
+
+    entry = {
+        "timestamp": timestamp,
+        "arm_dist": arm_dist,
+        "door_dist": door_dist,
+        "floor_dist": floor_dist,
+        "rope_mfl": rope_mfl,
+        "bearing_temp": bearing_temp,
+    }
+    entry["dsi"] = get_dsi(entry)
+    assert all(
+        component in entry for component in WEIGHTS.keys()
+    ), "Missing components in log entry"
+    return entry
+
+
+def generate_dataset(
+    log_frequency: Literal["hourly", "daily"] = "hourly",
+    start_date: datetime.datetime = START_DATE,
+    end_date: datetime.datetime = END_DATE,
+) -> pd.DataFrame:
+    """
+    Generates a dataset according to
+    `Proposal for Passenger Lift Remote Maintenance and Diagnostics (RM&D) Systems'
+    with the following columns:
+    - `timestamp': the date and time of the log entry
+    - `arm_dist': the distance between the arm and the shaft in mm
+    - `door_dist': the door gap in mm
+    - `floor_dist': the distance between the cab floor and landing in mm
+    - `rope_mfl': the magnetic flux leakage of the rope in mT
+    - `bearing_temp': the temperature of the bearing in deg C
+    """
+    # Generate timestamps based on the specified log frequency
+    timedelta = None
+    match log_frequency:
+        case "hourly":
+            timedelta = datetime.timedelta(hours=1)
+        case "daily":
+            timedelta = datetime.timedelta(days=1)
+        case _:
+            raise ValueError("invalid `log_frequency`. Must be 'hourly' or 'daily'.")
+    entries = []
+    for timestamp in pd.date_range(start_date, end_date, freq=timedelta):
+        entry = generate_entry(
+            timestamp=timestamp, start_date=start_date, end_date=end_date
+        )
+        entries.append(entry)
+    df = pd.DataFrame(entries)
+    return df
+
+
 if __name__ == "__main__":
-    lifts = generate_dataset()
-    export_dataset(lifts)
+    logs = generate_dataset()
+    print(logs.head(20))
+    export_dataset(logs, fname="liftdata_v2.csv")
+    # export_dataset(lifts)
